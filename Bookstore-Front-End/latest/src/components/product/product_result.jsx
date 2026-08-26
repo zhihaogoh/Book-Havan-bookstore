b26b1ce2302600557d90cf4b50173806163e1413
zhihaogoh <166619586+zhihaogoh@users.noreply.github.com>
2026-08-26 16:50:21 +0800

    result_product done on the static

--- Bookstore-Front-End/latest/src/components/product/product_result.jsx
+++ Bookstore-Front-End/latest/src/components/product/product_result.jsx
@@ -1,6 +1,8 @@
-import { Form } from "react-bootstrap";
+import PropTypes from "prop-types";
+import { Col, Form, Pagination, Row } from "react-bootstrap";
+import CardProduct from "../card_product/card_product";
 
-export default function ProductResult() {
+export default function ProductResult({ products }) {
   return (
     <>
       <div className="result_product">
@@ -9,8 +11,8 @@ export default function ProductResult() {
           <div className="sort">
             <span>Sort By : </span>
             <Form>
-              <Form.Group  controlId="formGridState">
-                  <Form.Select defaultValue="Most Popular">
+              <Form.Group controlId="formGridState">
+                <Form.Select defaultValue="Most Popular">
                   <option>Most Popular</option>
                   <option>Default</option>
                 </Form.Select>
@@ -18,7 +20,44 @@ export default function ProductResult() {
             </Form>
           </div>
         </div>
+        <div className="listing_product py-3">
+          <Row>
+            {products.map((item, index) => (
+              <Col xs={6} md={6} lg={4} key={index} className="mb-3">
+                <CardProduct product={item} />
+              </Col>
+            ))}
+          </Row>
+        </div>
+        <Pagination>
+          
+          <Pagination.Prev />
+          <Pagination.Item active>{1}</Pagination.Item>
+          <Pagination.Item>{2}</Pagination.Item>
+          <Pagination.Item>{3}</Pagination.Item>
+          <Pagination.Item>{4}</Pagination.Item>
+          <Pagination.Item>{5}</Pagination.Item>
+          <Pagination.Ellipsis />
+          <Pagination.Item>{22}</Pagination.Item>
+          <Pagination.Next />
+
+        </Pagination>
       </div>
     </>
   );
 }
+
+ProductResult.propTypes = {
+  products: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      img: PropTypes.string.isRequired,
+      BookName: PropTypes.string.isRequired,
+      Author: PropTypes.string.isRequired,
+      star: PropTypes.number.isRequired,
+      viewNumber: PropTypes.number.isRequired,
+      Price: PropTypes.number.isRequired,
+      discount: PropTypes.number,
+    }),
+  ).isRequired,
+};
--- Bookstore-Front-End/latest/src/pages/fliter_product/fliter_product.jsx
+++ Bookstore-Front-End/latest/src/pages/fliter_product/fliter_product.jsx
@@ -9,8 +9,8 @@ export default function FliterProduct() {
       <Layout>
         <div className="fliter_product container">
           <Row>
-            <Col xs={0} md={3.5} lg={4}>
-              <Fliter 
+            <Col xs={0} md={4} lg={4}>
+              <Fliter
                 check_categories={[
                   {
                     category: "Fiction",
@@ -38,8 +38,125 @@ export default function FliterProduct() {
                 ]}
               />
             </Col>
-            <Col xs={0} md={6} lg={8}>
-                <ProductResult />
+            <Col xs={0} md={8} lg={8}>
+              <ProductResult
+                products={[
+                  {
+                    id: 1,
+                    BookName: "The Silent Patient",
+                    Author: "Alex Michaelides",
+                    Price: 15.99,
+                    img: "../src/assets/Image/Product/Product6.jpg",
+                    viewNumber: 1000,
+                    star: 4.5,
+                  },
+                  {
+                    id: 2,
+                    BookName: "The Midnight Library",
+                    Author: "Matt Haig",
+                    Price: 14.99,
+                    img: "../src/assets/Image/Product/Product5.jpg",
+                    viewNumber: 800,
+                    star: 4.3,
+                  },
+                  {
+                    id: 3,
+                    BookName: "The Vanishing Half",
+                    Author: "Brit Bennett",
+                    Price: 16.99,
+                    img: "../src/assets/Image/Product/Product4.jpg",
+                    viewNumber: 1200,
+                    star: 4.7,
+                  },
+                  {
+                    id: 4,
+                    BookName: "The Four Winds",
+                    Author: "Kristin Hannah",
+                    Price: 17.99,
+                    img: "../src/assets/Image/Product/Product3.jpg",
+                    viewNumber: 900,
+                    star: 4.6,
+                  },
+                  {
+                    id: 5,
+                    BookName: "The Night Circus",
+                    Author: "Erin Morgenstern",
+                    Price: 13.99,
+                    img: "../src/assets/Image/Product/Product2.jpg",
+                    viewNumber: 1100,
+                    star: 4.4,
+                  },
+                  {
+                    id: 6,
+                    BookName: "The Seven Husbands of Evelyn Hugo",
+                    Author: "Taylor Jenkins Reid",
+                    Price: 14.99,
+                    img: "../src/assets/Image/Product/Product1.jpg",
+                    viewNumber: 1000,
+                    star: 4.5,
+                  },
+                  {
+                    id: 7,
+                    BookName: "The Silent Patient",
+                    Author: "Alex Michaelides",
+                    Price: 8.0,
+                    img: "../src/assets/Image/Product/Product6.jpg",
+                    viewNumber: 1000,
+                    star: 4.5,
+                    discount: 10,
+                  },
+                  {
+                    id: 8,
+                    BookName: "The Midnight Library",
+                    Author: "Matt Haig",
+                    Price: 7.5,
+                    img: "../src/assets/Image/Product/Product5.jpg",
+                    viewNumber: 800,
+                    star: 4.3,
+                    discount: 15,
+                  },
+                  {
+                    id: 9,
+                    BookName: "The Vanishing Half",
+                    Author: "Brit Bennett",
+                    Price: 8.5,
+                    img: "../src/assets/Image/Product/Product4.jpg",
+                    viewNumber: 1200,
+                    star: 4.7,
+                    discount: 15,
+                  },
+                  {
+                    id: 10,
+                    BookName: "The Four Winds",
+                    Author: "Kristin Hannah",
+                    Price: 8.5,
+                    img: "../src/assets/Image/Product/Product3.jpg",
+                    viewNumber: 900,
+                    star: 4.6,
+                    discount: 15,
+                  },
+                  {
+                    id: 11,
+                    BookName: "The Night Circus",
+                    Author: "Erin Morgenstern",
+                    Price: 7.0,
+                    img: "../src/assets/Image/Product/Product2.jpg",
+                    viewNumber: 1100,
+                    star: 4.4,
+                    discount: 15,
+                  },
+                  {
+                    id: 12,
+                    BookName: "The Seven Husbands of Evelyn Hugo",
+                    Author: "Taylor Jenkins Reid",
+                    Price: 7.5,
+                    img: "../src/assets/Image/Product/Product1.jpg",
+                    viewNumber: 1000,
+                    star: 4.5,
+                    discount: 15,
+                  },
+                ]}
+              />
             </Col>
           </Row>
         </div>
